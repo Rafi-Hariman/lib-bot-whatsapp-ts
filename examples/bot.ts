576c6c850caba694bb9b53da304ab3fc3383e422
Rafi-Hariman <rafi.hariman@uii.ac.id>
2024-11-11 10:28:12 +0700

    bug fix

--- examples/bot.ts
+++ examples/bot.ts
@@ -9,7 +9,6 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 const botBaileys = new BaileysClass({});
-let awaitingResponse = false;
 let userStep = 'initial_response';
 
 botBaileys.on('auth_failure', async (error) => console.log("ERROR BOT: ", error));
@@ -24,28 +23,27 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
-
-
 botBaileys.on('message', async (message) => {
-    const positiveResponses = require('../user-response/confirmResponse.json');
-
     const command = message.body.toLowerCase().trim();
- 
+
     switch (userStep) {
-        
         case 'initial_response':
-            await botBaileys.sendText(message.from, `Selamat datang di MenoPal. MenoPal adalah chatbot interaktif yang dapat membantu Anda dalam mengetahui dan menentukan gangguan pada menstruasi. Apakah Anda tertarik untuk menggunakan bot ini?`);
-            userStep = 'confirm_response';
+            await botBaileys.sendText(message.from, `Selamat datang di MenoPal. MenoPal adalah chatbot interaktif yang dapat membantu Anda dalam mengetahui dan menentukan gangguan pada menstruasi.`);
+
+            // Kirimkan polling untuk konfirmasi minat menggunakan bot
+            await botBaileys.sendPoll(message.from, 'Apakah Anda tertarik untuk menggunakan bot ini?', {
+                options: ['Iya', 'Tidak'],
+                multiselect: false
+            });
+            userStep = 'handle_poll_response';
             break;
 
-
-        case 'confirm_response':
-            if (positiveResponses.includes(command.toLowerCase())) {
+        case 'handle_poll_response':
+            if (command === 'iya') {
                 await botBaileys.sendText(
                     message.from,
                     `Baik, sebelum menggunakan fitur MenoPal ini, silahkan isi link berikut https://forms.gle/dLKUdo3gvzdCFqnL9.`
                 );
-
 
                 setTimeout(async () => {
                     await botBaileys.sendPoll(message.from, 'Apakah Anda sudah mengisi Form:', {
@@ -55,26 +53,26 @@ botBaileys.on('message', async (message) => {
                     userStep = 'form_confirmation';
                 }, 15 * 1000);
 
-            } else {
+            } else if (command === 'tidak') {
                 await botBaileys.sendText(
                     message.from,
                     'Terima kasih sudah menggunakan MenoPal. Semoga menstruasi anda tetap kondusif.'
                 );
                 userStep = 'end';
+            } else {
+                await botBaileys.sendText(message.from, 'Mohon pilih salah satu opsi: "Iya" atau "Tidak".');
             }
             break;
 
-
-
         case 'form_confirmation':
-            if (command.toLowerCase() === 'sudah') {
+            if (command === 'sudah') {
                 await botBaileys.sendText(message.from, `Terima kasih sudah mengisi form. Untuk mengetahui gangguan menstruasi, pilih kategori di bawah ini.`);
                 await botBaileys.sendPoll(message.from, 'Pilih Kategori:', {
                     options: ['Siklus', 'Volume', 'Lama', 'Gangguan Lain'],
                     multiselect: false
                 });
                 userStep = 'category_selection';
-            } else if (command.toLowerCase() === 'belum') {
+            } else if (command === 'belum') {
                 await botBaileys.sendText(message.from, 'Silakan isi form terlebih dahulu sebelum melanjutkan.');
                 userStep = 'end';
             } else {
@@ -121,11 +119,19 @@ botBaileys.on('message', async (message) => {
         // Siklus Category
         case 'siklus_category':
             if (command === 'siklus haid kurang dari 21 hari') {
-                await botBaileys.sendText(message.from, '“Gangguan menstruasi berupa Polimenorhea. polimenorhea merupakan ganguan menstruasi yang ditandai dengan siklus haid yang kurang dari 21 hari dan perdarahan yang lebih sedikit. Diperlukan konsulasi ke dokter jika anda mengalami tanda-tanda diatas. Ada yang ingin ditanyakan?”');
-                userStep = 'inquiry';
+                await botBaileys.sendText(message.from, 'Gangguan menstruasi berupa Polimenorhea. Polimenorhea merupakan gangguan menstruasi yang ditandai dengan siklus haid yang kurang dari 21 hari dan perdarahan yang lebih sedikit. Diperlukan konsulasi ke dokter jika Anda mengalami tanda-tanda di atas.');
+                await botBaileys.sendPoll(message.from, 'Ada yang ingin ditanyakan?', {
+                    options: ['Iya', 'Tidak'],
+                    multiselect: false
+                });
+                userStep = 'handle_inquiry_poll';
             } else if (command === 'siklus menstruasi lebih dari 35 hari') {
-                await botBaileys.sendText(message.from, '“Gangguan menstruasi berupa Oligomenorhea. Oligomenorrhea adalah Menstruasi yang jarang atau sangat sedikit, atau lebih tepatnya periode menstruasi yang berlangsung lebih dari 35 hari, disebut oligomenore. Berbagai kondisi, termasuk PCOS, Sindrom Prader-Will, fluktuasi hormon perimenopause, gangguan makan termasuk bulimia nervosa dan anoreksia nervosa, dan lain-lain, dapat menjadi penyebabnya.Diperlukan konsulasi ke dokter jika anda mengalami tanda-tanda diatas.Ada yang ingin ditanyakan?”');
-                userStep = 'inquiry';
+                await botBaileys.sendText(message.from, 'Gangguan menstruasi berupa Oligomenorhea. Oligomenorrhea adalah menstruasi yang jarang atau sangat sedikit, atau lebih tepatnya periode menstruasi yang berlangsung lebih dari 35 hari. Berbagai kondisi dapat menjadi penyebabnya. Diperlukan konsulasi ke dokter jika Anda mengalami tanda-tanda di atas.');
+                await botBaileys.sendPoll(message.from, 'Ada yang ingin ditanyakan?', {
+                    options: ['Iya', 'Tidak'],
+                    multiselect: false
+                });
+                userStep = 'handle_inquiry_poll';
             } else if (command === 'tidak menstruasi selama 3 bulan') {
                 await botBaileys.sendPoll(message.from, 'Apakah Anda pernah menstruasi sebelumnya?', {
                     options: ['Pernah', 'Tidak pernah'],
@@ -138,22 +144,38 @@ botBaileys.on('message', async (message) => {
         // Volume Category
         case 'volume_category':
             if (command === 'perdarahan banyak (lebih dari 8 hari)') {
-                await botBaileys.sendText(message.from, '“Gangguan menstruasi berupa Hipermonorea. Hipermenorea adalah kondisi medis yang ditandai dengan perdarahan haid yang lebih banyak atau lebih lama dari normal. Dan lamanya lebih dari 8 hari.Diperlukan konsulasi ke dokter jika anda mengalami tanda-tanda diatas.Ada yang ingin ditanyakan?”');
-                userStep = 'inquiry';
+                await botBaileys.sendText(message.from, 'Gangguan menstruasi berupa Hipermonorea. Hipermenorea adalah kondisi medis yang ditandai dengan perdarahan haid yang lebih banyak atau lebih lama dari normal. Diperlukan konsulasi ke dokter jika Anda mengalami tanda-tanda di atas.');
+                await botBaileys.sendPoll(message.from, 'Ada yang ingin ditanyakan?', {
+                    options: ['Iya', 'Tidak'],
+                    multiselect: false
+                });
+                userStep = 'handle_inquiry_poll';
             } else if (command === 'perdarahan sedikit (siklus menstruasi lebih dari 35 hari)') {
-                await botBaileys.sendText(message.from, '“Gangguan menstruasi berupa Oligomenorhea. Oligomenorrhea adalah Menstruasi yang jarang atau sangat sedikit, atau lebih tepatnya periode menstruasi yang berlangsung lebih dari 35 hari, disebut oligomenore. Berbagai kondisi, termasuk PCOS, Sindrom Prader-Will, fluktuasi hormon perimenopause, gangguan makan termasuk bulimia nervosa dan anoreksia nervosa, dan lain-lain, dapat menjadi penyebabnya.Diperlukan konsulasi ke dokter jika anda mengalami tanda-tanda diatas.Ada yang ingin ditanyakan?”');
-                userStep = 'inquiry';
+                await botBaileys.sendText(message.from, 'Gangguan menstruasi berupa Oligomenorhea. Oligomenorrhea adalah menstruasi yang jarang atau sangat sedikit, atau lebih tepatnya periode menstruasi yang berlangsung lebih dari 35 hari. Berbagai kondisi dapat menjadi penyebabnya. Diperlukan konsulasi ke dokter jika Anda mengalami tanda-tanda di atas.');
+                await botBaileys.sendPoll(message.from, 'Ada yang ingin ditanyakan?', {
+                    options: ['Iya', 'Tidak'],
+                    multiselect: false
+                });
+                userStep = 'handle_inquiry_poll';
             }
             break;
 
         // Lama Category
         case 'lama_category':
             if (command === 'lama menstruasi lebih dari 8 hari') {
-                await botBaileys.sendText(message.from, '“Gangguan menstruasi berupa Hipermonorea. Hipermenorea adalah kondisi medis yang ditandai dengan perdarahan haid yang lebih banyak atau lebih lama dari normal. Dan lamanya lebih dari 8 hari.Diperlukan konsulasi ke dokter jika anda mengalami tanda-tanda diatas.Ada yang ingin ditanyakan?”');
-                userStep = 'inquiry';
+                await botBaileys.sendText(message.from, 'Gangguan menstruasi berupa Hipermonorea. Hipermenorea adalah kondisi medis yang ditandai dengan perdarahan haid yang lebih banyak atau lebih lama dari normal. Diperlukan konsulasi ke dokter jika Anda mengalami tanda-tanda di atas.');
+                await botBaileys.sendPoll(message.from, 'Ada yang ingin ditanyakan?', {
+                    options: ['Iya', 'Tidak'],
+                    multiselect: false
+                });
+                userStep = 'handle_inquiry_poll';
             } else if (command === 'lama menstruasi kurang dari 3 hari') {
-                await botBaileys.sendText(message.from, '“Gangguan menstruasi berupa Hipomenorea.Hipomenorea adalah perdarahan haid yang lebih sedikit dari biasanya yaitu terjadinya perdarahan menstruasi yang lebih sedikit dari volume normal dan lamanya kurang dari 3 hari.Diperlukan konsulasi ke dokter jika anda mengalami tanda-tanda diatas.Ada yang ingin ditanyakan?”');
-                userStep = 'inquiry';
+                await botBaileys.sendText(message.from, 'Gangguan menstruasi berupa Hipomenorea. Hipomenorea adalah perdarahan haid yang lebih sedikit dari biasanya, yaitu terjadinya perdarahan menstruasi yang lebih sedikit dari volume normal dan lamanya kurang dari 3 hari. Diperlukan konsulasi ke dokter jika Anda mengalami tanda-tanda di atas.');
+                await botBaileys.sendPoll(message.from, 'Ada yang ingin ditanyakan?', {
+                    options: ['Iya', 'Tidak'],
+                    multiselect: false
+                });
+                userStep = 'handle_inquiry_poll';
             }
             break;
 
@@ -183,7 +205,11 @@ botBaileys.on('message', async (message) => {
                     '7. Obat pereda nyeri\n\n' +
                     'Ada yang ingin ditanyakan?”'
                 );
-                userStep = 'inquiry';
+                await botBaileys.sendPoll(message.from, 'Ada yang ingin ditanyakan?', {
+                    options: ['Iya', 'Tidak'],
+                    multiselect: false
+                });
+                userStep = 'handle_inquiry_poll';
             } else if (command === 'malas bergerak, nyeri payudara, muncul jerawat, nafsu makan meningkat') {
                 await botBaileys.sendText(message.from,
                     '“Gangguan menstruasi berupa Premenstrual Syndrome (PMS).\n\n' +
@@ -215,26 +241,38 @@ botBaileys.on('message', async (message) => {
                     'PMS sulit dihindari karena tidak ada yang tahu apa penyebabnya. Mempertahankan gaya hidup sehat adalah pendekatan paling efektif untuk menurunkan peluang Anda terkena PMS.\n\n' +
                     'Ada yang ingin ditanyakan?”'
                 );
-                userStep = 'inquiry';
+                await botBaileys.sendPoll(message.from, 'Ada yang ingin ditanyakan?', {
+                    options: ['Iya', 'Tidak'],
+                    multiselect: false
+                });
+                userStep = 'handle_inquiry_poll';
             }
             break;
-
 
         // Amenorrhea Check
         case 'amenorrhea_check':
             if (command === 'pernah') {
-                await botBaileys.sendText(message.from, '“Gangguan menstruasi berupa Amenenorhea sekunder. Amemenorrhea sekunder adalah,  kondisi Ketika seorang Wanita yang sudah pernah mengalami menstruasi tidak menstruasi selama lebih dari tiga bulan berturu-turut.Diperlukan konsulasi ke dokter jika anda mengalami tanda-tanda diatas.Ada yang ingin ditanyakan?”');
+                await botBaileys.sendText(message.from, 'Gangguan menstruasi berupa Amenenorhea sekunder. Amemenorrhea sekunder adalah kondisi ketika seorang wanita yang sudah pernah mengalami menstruasi tidak menstruasi selama lebih dari tiga bulan berturut-turut. Diperlukan konsulasi ke dokter jika Anda mengalami tanda-tanda di atas.');
             } else {
-                await botBaileys.sendText(message.from, '“Gangguan menstruasi berupa Amemenorhea primer Amenorea primer adalah kondisi ketika seorang wanita belum m engalami menstr asi pertamanya pada usia 15 tahun atau 3 tahun setelah menarche, meskipun sudah mengalami perkembangan normal dan karakteristik seksual sekunderDiperlukan konsulasi ke dokter jika anda mengalami tanda-tanda diatas.Ada yang ingin ditanyakan?”');
+                await botBaileys.sendText(message.from, 'Gangguan menstruasi berupa Amemenorhea primer. Amenorea primer adalah kondisi ketika seorang wanita belum mengalami menstruasi pertamanya pada usia 15 tahun atau 3 tahun setelah menarche. Diperlukan konsulasi ke dokter jika Anda mengalami tanda-tanda di atas.');
             }
-            userStep = 'inquiry';
+            await botBaileys.sendPoll(message.from, 'Ada yang ingin ditanyakan?', {
+                options: ['Iya', 'Tidak'],
+                multiselect: false
+            });
+            userStep = 'handle_inquiry_poll';
             break;
 
-        // Inquiry Handling
-        case 'inquiry':
-            if (positiveResponses.includes(command)) {
-                await botBaileys.sendText(message.from, 'Baik, MenoPal akan menyambungkan ke customer service. Mohon tunggu.');
-                userStep = 'customer_service';
+        // Sending Poll for Inquiry
+
+
+        // Handling Inquiry Poll
+        case 'handle_inquiry_poll':
+            if (command === 'iya') {
+                await botBaileys.sendText(
+                    message.from,
+                    `Baik, MenoPal akan menyambungkan ke customer service. Mohon tunggu.`
+                );
                 setTimeout(async () => {
                     await botBaileys.sendText(
                         message.from,
@@ -242,29 +280,34 @@ botBaileys.on('message', async (message) => {
                     );
                 }, 5000);
                 userStep = 'awaiting_user_question';
-            } else {
+            } else if (command === 'tidak') {
                 await botBaileys.sendText(message.from, 'Terima kasih telah menggunakan MenoPal. Anda dapat kembali ke menu utama jika ingin memeriksa kategori lainnya.');
-                userStep = 'end';
-                await botBaileys.sendText(
-                    message.from,
-                    `Agar kami mengetahui lebih lanjut tentang menstruasi Anda, kami berharap Anda dapat mengisi survei berikut:\n` +
-                    `https://forms.gle/NBRcZ1yBZHuvnLgs5`
-                );
+                await botBaileys.sendPoll(message.from, 'Apakah anda ingin kembali ke menu?', {
+                    options: ['Iya', 'Tidak'],
+                    multiselect: false
+                });
+                userStep = 'handle_menu_return';
+            } else {
+                await botBaileys.sendPoll(message.from, 'Ada yang ingin ditanyakan?', {
+                    options: ['Iya', 'Tidak'],
+                    multiselect: false
+                });
             }
             break;
 
-        case 'awaiting_user_question':
-            if (command) {
+        case 'handle_menu_return':
+            if (command === 'iya') {
+                await botBaileys.sendPoll(message.from, 'Pilih Kategori:', {
+                    options: ['Siklus', 'Volume', 'Lama', 'Gangguan Lain'],
+                    multiselect: false
+                });
+                userStep = 'category_selection';
+            } else if (command === 'tidak') {
                 await botBaileys.sendText(
                     message.from,
-                    `Terima kasih atas pertanyaannya. Kami sedang meninjau dan akan memberikan informasi yang relevan secepatnya.`
+                    'Terima kasih telah menggunakan MenoPal. Semoga hari Anda menyenangkan. Jika anda berkenan mengisi survei mengenai menstruasi perempuan , silakan klik link berikut: https://forms.gle/NBRcZ1yBZHuvnLgs5'
                 );
                 userStep = 'end';
-            } else {
-                await botBaileys.sendText(
-                    message.from,
-                    `Maaf, kami belum menerima pertanyaan yang spesifik. Silakan tanyakan apa yang ingin Anda ketahui.`
-                );
             }
             break;
 
@@ -286,23 +329,13 @@ botBaileys.on('message', async (message) => {
         case 'end':
             await botBaileys.sendText(
                 message.from,
-                `Terimakasih atas waktunya, semoga hari anda menyenangkan.`
+                `Terima kasih atas waktunya, semoga hari anda menyenangkan.`
             );
             userStep = 'initial_response';
             break;
-
-        // case 'survey':
-        //     await botBaileys.sendText(
-        //         message.from,
-        //         `Agar kami mengetahui lebih lanjut tentang menstruasi Anda, kami berharap Anda dapat mengisi survei berikut:\n` +
-        //         `https://forms.gle/NBRcZ1yBZHuvnLgs5`
-        //     );
-        //     userStep = 'initial_response';
-        //     break;
 
         default:
             await botBaileys.sendText(message.from, 'Jika ada yang ingin ditanyakan, silakan ketik tanya.');
             break;
     }
 });
-
